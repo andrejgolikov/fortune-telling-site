1a3d18fad648cb48ddf9369882f6360f5a16b09c
Andrey Golikov <golickovandrey@gmail.com>
2020-11-15 10:26:25 +0300

    Deploy file added

---
+++ deploy.php
@@ -0,0 +1,50 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'fortune-telling-site');
+
+// Project repository
+set('repository', 'https://github.com/andrejgolikov/fortune-telling-site.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+set('shared_files', []);
+set('shared_dirs', []);
+
+// Writable dirs by web server 
+set('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+host('vh324.timeweb.ru')
+    ->user('cq04154')
+    ->set('deploy_path', '/home/c/cq04154/production');
+    
+
+// Tasks
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:vendors',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');

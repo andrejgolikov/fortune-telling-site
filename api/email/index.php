ca1c89ff0f996d595cabea07d081b9e882aa75e0
Andrey Golikov <golickovandrey@gmail.com>
2020-12-02 11:24:04 +0300

    Need help email form: Sending emails returned to using PHP's mail function

--- api/email/index.php
+++ api/email/index.php
@@ -1,30 +1,14 @@
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-
-// Load Composer's autoloader
-require '../../vendor/autoload.php';
-
-// Instantiation and passing `true` enables exceptions
-$mail = new PHPMailer();
-
-//Recipients
-//$mail->setFrom('andrejgolickov@yandex.ru', 'Mailer');
-//Set who the message is to be sent to
-$mail->addAddress('andrejgolickov@yandex.ru', 'John Doe');
-//Set the subject line
-$mail->Subject = 'mira-gadaniya.ru: Отправлена форма "Нужна помощь"';
-//Replace the plain text body with one created manually
-$mail->AltBody = 'Поля:\nТелефон: ' . $_POST['phone'] . '\nEmail: ' . $_POST['email'] . '\nСообщение: ' . $_POST['message'];
-
-//send the message, check for errors
-if (!$mail->send()) {
-    $responseData = [
-        'error' => $mail->ErrorInfo,
-    ];
-} else {
-    $responseData = [];
-}
-
-echo json_encode($responseData);
+echo json_encode([
+    'data' => [
+        'result' => $result = mail(
+            'mirakulpieva@yandex.ru',
+            'mira-gadaniya.ru: Отправлена форма "Нужна помощь"',
+            'Поля:'
+            . "\n" . 'Дата: ' . date('d.m.Y H:i:s')
+            . "\n" . 'Телефон: ' . $_POST['phone']
+            . "\n" . 'Email: ' . $_POST['email']
+            . "\n" . 'Сообщение: ' . $_POST['message']
+        )
+    ]
+]);
